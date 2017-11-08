79cfea7cdc5c3f3b859ba1e8c1207f734d415ddb
nweese <nweese@ymail.com>
2017-11-07 16:33:37 -0800

    functional application

--- app.js
+++ app.js
@@ -5,40 +5,42 @@ var points = 0;
 
 var yesCorrect = function(results) {
   if (results.toLowerCase() === "yes" || results.toLowerCase() === "y") {
-    return true;
-  } else if (results.toLowerCase() !== "no" || results.toLowerCase() !== "n") {
-    return null;
+    points += 10;
+    alert('Woo! Way to go! You have ' + points + ' points, you rock!');
+  } else if (results.toLowerCase() === "no" || results.toLowerCase() === "n") {
+    alert('You really don\'t know me at all, do you? Keep going! Better luck next question!');
+  } else {
+    alert('Excuse me! That is not a valid answer! Please try harder next time.');
+  }
+}
+
+var noCorrect = function(results) {
+  if (results.toLowerCase() === "no" || results.toLowerCase() === "n") {
+    points += 10;
+    alert('Woo! Way to go! You have ' + points + ' points, you rock!');
+  } else if (results.toLowerCase() === "yes" || results.toLowerCase() === "y") {
+    alert('You really don\'t know me at all, do you? Keep going! Better luck next question!');
+  } else {
+    alert('Excuse me! That is not a valid answer! Please try harder next time.');
   }
 }
 
 var questionOne = prompt('Do I like pina coladas?')
-if (yesCorrect(questionOne) === true) {
-  points +=10;
-} else if (yesCorrect(questionOne) === null) {
-  alert('Excuse me! That is not a valid answer! Please try harder next time.')
-}
+yesCorrect(questionOne);
 console.log("user points after question one", points);
 
 var questionTwo = prompt('Do I like getting lost in the rain?')
-if (yesCorrect(questionTwo) === false) {
-  points +=10;
-}
+noCorrect(questionTwo);
 console.log("user points after question two", points);
 
 var questionThree = prompt('Am I into yoga?')
-if (yesCorrect(questionThree) === false) {
-  points +=10;
-}
+noCorrect(questionThree);
 console.log("user points after question three", points);
 
 var questionFour = prompt('Do I have half a brain?')
-if (yesCorrect(questionFour) === true) {
-  points +=10;
-}
+yesCorrect(questionFour);
 console.log("user points after question four", points);
 
 var questionFive = prompt('Do I actually like this song?')
-if (yesCorrect(questionFive) === true) {
-  points +=10;
-}
+yesCorrect(questionFive);
 console.log("user points after question five", points);
