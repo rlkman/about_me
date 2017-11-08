f1db41c11fa3ba4ffa1d6d5446617006cf8b13d5
nweese <nweese@ymail.com>
2017-11-07 16:53:23 -0800

    added functions for different possible messages for efficiency

--- app.js
+++ app.js
@@ -3,27 +3,39 @@
 
 var points = 0;
 
+var winningMessage = function() {
+  points += 10;
+  alert('Woo! Way to go! You have ' + points + ' points, you rock!');
+}
+var losingMessage = function() {
+  alert('You really don\'t know me at all, do you? Keep going! Better luck next question!');
+}
+
+var wrongInputMessage = function() {
+  alert('Excuse me! That is not a valid answer! Please try harder next time.');
+}
+
 var yesCorrect = function(results) {
   if (results.toLowerCase() === "yes" || results.toLowerCase() === "y") {
-    points += 10;
-    alert('Woo! Way to go! You have ' + points + ' points, you rock!');
+    winningMessage();
   } else if (results.toLowerCase() === "no" || results.toLowerCase() === "n") {
-    alert('You really don\'t know me at all, do you? Keep going! Better luck next question!');
+    losingMessage();
   } else {
-    alert('Excuse me! That is not a valid answer! Please try harder next time.');
+    wrongInputMessage();
   }
 }
 
 var noCorrect = function(results) {
   if (results.toLowerCase() === "no" || results.toLowerCase() === "n") {
-    points += 10;
-    alert('Woo! Way to go! You have ' + points + ' points, you rock!');
+    winningMessage();
   } else if (results.toLowerCase() === "yes" || results.toLowerCase() === "y") {
-    alert('You really don\'t know me at all, do you? Keep going! Better luck next question!');
+    losingMessage();
   } else {
-    alert('Excuse me! That is not a valid answer! Please try harder next time.');
+    wrongInputMessage();
   }
 }
+
+alert('Welcome! By visiting this webpage you have unknowingly subjected yourself to my guessing game. Nobody cares if you want to play or not, you are here now. Let the games begin! You will receive 10 points for a correct answer, and no points for a wrong answer (duh). Lets see how many points you can get before the game is over!')
 
 var questionOne = prompt('Do I like pina coladas?')
 yesCorrect(questionOne);
@@ -44,3 +56,11 @@ console.log("user points after question four", points);
 var questionFive = prompt('Do I actually like this song?')
 yesCorrect(questionFive);
 console.log("user points after question five", points);
+
+if (points === 50) {
+  alert('Congratulations! You finished with ' + points + "!");
+} else if (points >= 30) {
+  alert('Maybe try again. I could play this game all day.');
+} else {
+  alert('You suck. We\'re not friends anymore. Bye!');
+}
